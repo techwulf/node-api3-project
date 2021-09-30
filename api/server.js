0a283c9a50dc569a27f86f1ad9a3c3ad4549a1b5
techwulf <t.zach.moore@gmail.com>
2021-09-29 21:37:36 -0600

    implement get /api/users/:id route

--- api/server.js
+++ api/server.js
@@ -13,4 +13,10 @@ server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
+server.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    message: err.message
+  });
+});
+
 module.exports = server;
